0bbc841eb22174a14902253923edb4f08fed22b0
utsaho <pipul.boddyo@gmail.com>
2026-01-05 12:08:25 +0600

    UPDATE: on ad

--- src/app/components/layout/header/announcementBar.tsx
+++ src/app/components/layout/header/announcementBar.tsx
@@ -7,12 +7,6 @@ const AnnouncementBar = () => {
                 <div className="absolute inset-0 opacity-30">
                     <div className="w-full h-full bg-[url('/images/announcementbar/announcementbar-bg.jpg')] bg-cover bg-center bg-no-repeat" />
                 </div>
-                <div className="relative z-10 container">
-                    <div className="py-2.5 flex items-center justify-center gap-2">
-                        <p className="text-sm sm:text-base text-white">Now available on Figma & Code — start customizing your personal site today.</p>
-                        <Image src={"/images/icon/arrow-icon.svg"} alt="arrow-icon" width={24} height={24} className="group-hover:translate-x-1.5 transition-all duration-500 ease-in-out" />
-                    </div>
-                </div>
             </div>
         </div>
     )
